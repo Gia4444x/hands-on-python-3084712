d3864649475a265e8e5c378f6f3ed313c2719dcf
Gia4444x <85368056+Gia4444x@users.noreply.github.com>
2025-01-03 23:58:35 +0000

    NCT DREAM

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -1,18 +1,25 @@
-NAMES = ["John", "Paul", "George", "Ringo"]
-AGES = [20, 21, 22, 23]
+import statistics as s
 
-JOHN = NAMES[0]
-PAUL = NAMES[1]
+NAMES = ["Mark", "Renjun", "Jeno", "Haechan", "Jaemin", "Chenle", "Jisung"]
+AGES = [25, 24, 24, 24, 24, 23, 22]
 
-JOHN_PAUL = NAMES[:2]
-GEORGE_RINGO = NAMES[2:]
+MK = NAMES[0]
+RJ = NAMES[1]
+
+MK_RJ = NAMES[:2]
+JN_HC = NAMES[2:]
+maknaes = NAMES[-2:]
+INFJazz = [MK, NAMES[-1]]
 REVERSE = NAMES[::-1]
 EVERY_OTHER = NAMES[::2]
 
 print(sum(AGES))
 print(min(AGES))
 print(max(AGES))
+print(round(s.mean(AGES), 2))
 
-print(JOHN_PAUL)
-print(GEORGE_RINGO)
+print(maknaes)
+print(INFJazz)
 print(REVERSE)
+
+print(f"Show me who's the King!\n {NAMES[0]}")
